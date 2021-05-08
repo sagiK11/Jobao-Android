c997126f0b84de894c43c4b9a61428e09eef0ad9
Sagi Korzack <37194830+sagiK11@users.noreply.github.com>
2021-05-08 12:19:49 +0300

    :bug: bug fix: keyboard hides several input fields

--- app/src/main/java/com/sagikor/android/jobao/ui/activities/MainActivity.kt
+++ app/src/main/java/com/sagikor/android/jobao/ui/activities/MainActivity.kt
@@ -2,12 +2,9 @@ package com.sagikor.android.jobao.ui.activities
 
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
@@ -106,15 +103,6 @@ class MainActivity : AppCompatActivity(), OnScrollListener {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
-        if (currentFocus != null) {
-            val imm: InputMethodManager =
-                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
-        }
-        return super.dispatchTouchEvent(ev)
     }
 
     override fun onScrollUp() {
